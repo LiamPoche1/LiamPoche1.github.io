8c7508f826fba1d3d6f3c84e8de13a60c901fd4e
LiamPoche1 <Liampoche79@gmail.com>
2026-06-05 14:40:24 +0000

    saved work so far

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -35,19 +35,45 @@ $(document).ready(function () {
   /////////////////////////////////////////////////
 
   // TODO 1: create a new shape object and add it to the array
-  
+  var shape = {
+    color: "blue",
+    shape: "circle",
+    repeat: 3,
+  };
+  dataShapes.push(shape);
 
   // TODO 2: add a new property to all data shapes
-  
+  for (i = 0; i < dataShapes.length; i++) {
+    currentShape = dataShapes[i];
+    if (currentShape.color === "red") {
+      currentShape.goodBehavior = "bounce";
+    } else if (currentShape.color === "blue") {
+      currentShape.goodBehavior = "blink";
+    } else if (currentShape.color === "green") {
+      currentShape.goodBehavior = "spin";
+    }
+  }
 
   // TODO 3-a: add a function that handles the static display type
-  
+
+  function handleStatic(data) {
+    setBackgroundWithObject(data);
+    //data//
+    animationDetails.displayType = 1;
+  }
 
   // TODO 4-a: add a function that handles the good display type
-  
+  function handleGood(color, shape, repeat) {
+    setBackgroundWithSimple(color, shape, repeat);
+    animationDetails.displayType = 2;
+  }
 
   // TODO 5-a: add a function that handles the bad display type
-  
+  function handleBad(data, repeat) {
+    repeat = repeat + 1;
+    setBackgroundsWithMixed(data, repeat);
+    animationDetails.displayType = 3;
+  }
 
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
@@ -55,17 +81,20 @@ $(document).ready(function () {
 
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
-    
+    handleStatic(dataShapes[currentIndex]);
   }
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    
+    var currentShape = dataShapes[currentIndex];
+    handleGood(currentShape.color, currentShape.shape, currentShape.repeat);
   }
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
-    
+    handleBad(currentshape, currentShape.repeat);
+    var currentShape = dataShapes[currentIndex];
+    repeat = currentShape.repeat;
   }
 
   /////////////////////////////////////////////////
@@ -118,7 +147,7 @@ $(document).ready(function () {
   // This function resets the display type to the default display (only shows data)
   function resetDisplay() {
     const shapeData = dataShapes[currentIndex];
-
+    console.log(shapeData);
     // Reset all of the CSS and HTML
     $("#shape").css("background", "none");
     $("#shape").css("display", "block");
@@ -127,7 +156,7 @@ $(document).ready(function () {
     $("#shape").css("top", "150px");
     $("#shape").css("transform", "rotate(0deg)");
     $("#shape").html(
-      `<p>${shapeData.color}</p> <p>${shapeData.shape}</p> <p>${shapeData.repeat}x${shapeData.repeat}</p> <p>${shapeData.goodBehavior}</p>`
+      `<p>${shapeData.color}</p> <p>${shapeData.shape}</p> <p>${shapeData.repeat}x${shapeData.repeat}</p> <p>${shapeData.goodBehavior}</p>`,
     );
 
     $("#info-bar").text(`Current index: ${currentIndex}`);
